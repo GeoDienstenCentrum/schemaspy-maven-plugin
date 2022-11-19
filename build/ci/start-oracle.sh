2fcf42e35a87b561508b292f0b136651542d3629
Mark Prins <1165786+mprins@users.noreply.github.com>
2022-11-19 14:31:48 +0100

    parametrize oracle database version

--- build/ci/start-oracle.sh
+++ build/ci/start-oracle.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-# Oracle XE 21
-# this docker image has the following users/credentials (user/password = system/oracle)
-docker pull ghcr.io/gvenzl/oracle-xe:21.3.0
+# Oracle XE
+docker pull ghcr.io/gvenzl/oracle-xe:$1
 
 # start the dockerized oracle-xe instance
-# this container can be stopped using:
+# this container can be stopped/removed using:
 #
 #    docker stop schemaspy
 #
-docker run --rm -p 1521:1521 --name schemaspy -h schemaspy -e ORACLE_PASSWORD=oracle -d ghcr.io/gvenzl/oracle-xe:21.3.0
+# this container has the following admin user/credentials (user/password = system/oracle)
+docker run --rm -p 1521:1521 --name schemaspy -h schemaspy -e ORACLE_PASSWORD=oracle -d ghcr.io/gvenzl/oracle-xe:$1
 
 printf "\n\nStarting Oracle XE container, this could take a few minutes..."
 printf "\nWaiting for Oracle XE database to start up.... "
@@ -27,7 +27,3 @@ do
     sleep 10
     _WAIT=$(($_WAIT+10))
 done
-
-# docker ps -a
-# print logs
-#docker logs schemaspy
