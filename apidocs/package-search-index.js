3bc3292ea1c1da57bace6637eea9458406c2da39
mark <1165786+mprins@users.noreply.github.com>
2025-01-22 10:58:40 +0100

    Site checkin for project SchemaSpy Maven Plugin

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.wakaleo.schemaspy"},{"l":"com.wakaleo.schemaspy.util"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.wakaleo.schemaspy"},{"l":"com.wakaleo.schemaspy.util"}];updateSearchResults();
