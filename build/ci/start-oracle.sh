ef8923528e82048b56914830678553398a1b9c2d
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-04-03 16:01:09 +0200

    Use Oracle Free database image
    close #708
    
    try the slim image

--- build/ci/start-oracle.sh
+++ build/ci/start-oracle.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
-# Oracle XE
-docker pull ghcr.io/gvenzl/oracle-xe:$1
+# Oracle FREE
+docker pull ghcr.io/gvenzl/oracle-free:$1
 
-# start the dockerized oracle-xe instance
+# start the dockerized oracle-free instance
 # this container can be stopped/removed using:
 #
 #    docker stop schemaspy
 #
 # this container has the following admin user/credentials (user/password = system/oracle)
-docker run --rm -p 1521:1521 --name schemaspy -h schemaspy -e ORACLE_PASSWORD=oracle -d ghcr.io/gvenzl/oracle-xe:$1
+docker run --rm -p 1521:1521 --name schemaspy -h schemaspy -e ORACLE_PASSWORD=oracle -d ghcr.io/gvenzl/oracle-free:$1
 
-printf "\n\nStarting Oracle XE container, this could take a few minutes..."
-printf "\nWaiting for Oracle XE database to start up.... "
+printf "\n\nStarting Oracle FREE container, this could take a few minutes..."
+printf "\nWaiting for Oracle FREE database to start up.... "
 _WAIT=0;
 while :
 do
     printf " $_WAIT"
     if $(docker logs schemaspy | grep -q 'DATABASE IS READY TO USE!'); then
-        printf "\nOracle XE Database started\n\n"
+        printf "\nOracle FREE Database started\n\n"
         break
     fi
     if ((_WAIT > 150)); then
-      printf "\nWaited >150 seconds for Oracle XE Database to start\n\n"
+      printf "\nWaited >150 seconds for Oracle FREE Database to start\n\n"
       break
     fi
     sleep 10
